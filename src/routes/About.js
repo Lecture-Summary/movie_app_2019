7da14881ba5330d54d2913123df6a17f205ba30e
HoJin <kiss0104040@naver.com>
2020-01-05 23:03:43 +0900

    About css

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-  return <span>About this page: I built it because I love movies.</span>;
+  return (
+    <div className="about_container">
+      <span>"Freedom is the freedom to say that two plus two make four."</span>
+      <span>− George Orwell, 1984</span>
+    </div>
+  );
 }
 
 export default About;
