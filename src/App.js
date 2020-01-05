d670880d49496acbf99bbb70648a9f20f4327444
HoJin <kiss0104040@naver.com>
2020-01-05 22:36:24 +0900

    CSS Style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
   return (
